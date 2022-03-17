4846e2e59619989cd63944de5fc38578aba92434
Kdvergarab <kdvergarab@gmail.com>
2022-03-17 02:43:04 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+pronoun.map(inicio => {
+  adj.map(medio => {
+    noun.map(fin => {
+      document.getElementById("ejem").innerHTML +=
+        inicio + medio + fin + ".com <br/>";
+    });
+  });
+});
